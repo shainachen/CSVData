6e686fbd654494286df21f9628cba9a88a3a0bc2
schen625 <schen625@fhs.win.fuhsd.org>
2016-10-14 10:56:52 -0700

    Create CSVDataTester class

---
+++ src/CSVDataTester.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class CSVDataTester {
+	public static void main(String[] args){
+		String[] columnNames={"time", "gyro-x", "gyro-y", "gyro-z"};
+		CSVData test=new CSVData("E:\\Shaina\\11th grade\\AP Comp Sci\\GyroTest2out.csv",columnNames,1 );
+		
+		System.out.println(Arrays.toString(test.getRow(1)));
+	}
+}
